4243dbc816b8f3369a22390a31ea148269b5e7d3
August Schau <schaua@augustschau.com>
2025-03-21 16:49:32 -0400

    Update my barrier demo

--- src/module07/barrier.cpp
+++ src/module07/barrier.cpp
@@ -3,19 +3,35 @@
 #include <barrier>
 #include <vector>
 
-void schedule_task(int id, std::barrier<>& sync_point) {
-    sync_point.arrive_and_wait();
-    std::cout << "Task " << id << " is scheduled.\n";
-}
+// Moving the thread logic to access the barrier directly
+// solved the runtime error on the thread logic being 
+// callable.
+
+
+// void schedule_task(int id, std::barrier<>& sync_point) {
+//     sync_point.arrive_and_wait();
+//     std::cout << "Task " << id << " is scheduled.\n";
+// }
+
 
 int main() {
     const int num_threads = 5;
-    auto on_completion = []() noexcept { std::cout << "All tasks are ready to be scheduled." << std::endl; };
+
+    auto on_completion = []() noexcept 
+    {
+         std::cout << "All tasks are ready to be scheduled." << std::endl; 
+    };
+    
     std::barrier sync_point(num_threads, on_completion);
+
+    auto schedule_task = [&](int id){
+        sync_point.arrive_and_wait();
+        std::cout << "Task " << id << " is scheduled.\n";
+    };
 
     std::vector<std::thread> threads;
     for (int i = 0; i < num_threads; ++i) {
-        threads.emplace_back(schedule_task, i, std::ref(sync_point));
+        threads.emplace_back(schedule_task, i);
     }
 
     for (auto& t : threads) {
